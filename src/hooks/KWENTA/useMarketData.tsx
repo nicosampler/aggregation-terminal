ddbd01411bc25ce771a5b93b54c3811acdc57304
Marcelo Rubini <rubini.marcelo@gmail.com>
2023-03-29 01:38:45 -0300

    reduces kwenta position details fetching time

--- src/hooks/KWENTA/useMarketData.tsx
+++ src/hooks/KWENTA/useMarketData.tsx
@@ -2,7 +2,9 @@ import Wei, { wei } from '@synthetixio/wei'
 
 import { useContractCall } from '../useContractCall'
 import { useReadContractInstance } from '../useContractInstance'
+import { MarketData } from './useMarketInternal'
 import { zeroBN } from './useMarketPrice'
+import { MarketParams } from './useMarketSettings'
 import { Chains } from '@/src/config/web3'
 import { FuturesMarketAsset, FuturesMarketKey } from '@/src/utils/KWENTA/constants'
 import { PerpsV2MarketData, PerpsV2MarketData__factory } from '@/types/generated/typechain'
@@ -68,33 +70,14 @@ export function useFetchMarket(marketKeyBytes: string) {
   return market
 }
 
-export function extractMarketInfo(market: FuturesMarket, assetRate: Wei) {
+export function extractMarketInfo(marketData: MarketData, marketParams: MarketParams) {
   // focused on oneHourlyFundingRate, skewAdjustedPrice
 
   // oneHourlyFundingRate
-  const oneHourlyFundingRate = market.currentFundingRate // wei(futuresMarket.currentFundingRate).div(24)
-
-  // double check if this calculation is really necessary
-  const skewAdjustedPrice = assetRate
-    ? wei(assetRate).mul(wei(market.marketSkew).div(market.settings.skewScale).add(1))
+  const oneHourlyFundingRate = wei(marketData.currentFundingRate).div(24) // wei(futuresMarket.currentFundingRate).div(24)
+  const skewAdjustedPrice = marketData.assetPrice
+    ? wei(marketData.assetPrice).mul(wei(marketData.marketSkew).div(marketParams.skewScale).add(1))
     : zeroBN
 
   return { oneHourlyFundingRate, skewAdjustedPrice }
 }
-
-// export function useFetchGlobals() {
-//   const reader = useReadContractInstance(
-//     Chains.optimism,
-//     PerpsV2MarketData__factory,
-//     'KWENTA_PerpsV2MarketData',
-//   )
-//   const calls = [reader.globals] as const
-
-//   const res = useContractCall<PerpsV2MarketData, typeof calls>(
-//     calls,
-//     [[]],
-//     `KWENTA_PerpV2MarketData_globals_${Chains.optimism}`,
-//   )
-
-//   return !res[0].data ? {} : res[0].data[0]
-// }
--- src/pagePartials/KWENTAStats.tsx
+++ src/pagePartials/KWENTAStats.tsx
@@ -3,16 +3,12 @@ import { Dispatch, memo } from 'react'
 import { wei } from '@synthetixio/wei'
 import { formatBytes32String } from 'ethers/lib/utils'
 
-import { useFetchCurrentRoundId, useRatesForCurrencies } from '../hooks/KWENTA/useExchangeRates'
-import { extractMarketInfo, useFetchMarket } from '../hooks/KWENTA/useMarketData'
-import { useFetchParameters } from '../hooks/KWENTA/useMarketSettings'
-import { useSynthsRates } from '../hooks/KWENTA/useSynthsRates'
-import { contracts } from '@/src/contracts/contracts'
-import { getMarketPrices } from '@/src/hooks/KWENTA/useMarketPrice'
-import { useGetTradePreview } from '@/src/hooks/KWENTA/usePositionDetails'
-import useProtocols from '@/src/hooks/useProtocols'
+import { extractMarketInfo } from '../hooks/KWENTA/useMarketData'
+import { useMarketInternalV2Data } from '../hooks/KWENTA/useMarketInternal'
+import { useMarketSettingsV2Parameters } from '../hooks/KWENTA/useMarketSettings'
+import { useTradePreview } from '../hooks/KWENTA/usePositionDetails'
 import { FuturesMarketKey, KWENTA_FIXED_FEE } from '@/src/utils/KWENTA/constants'
-import { formatFuturesMarket, formatOrderSizes, formatPosition } from '@/src/utils/KWENTA/format'
+import { formatOrderSizes, formatPosition } from '@/src/utils/KWENTA/format'
 import { ChainsValues } from '@/types/chains'
 import { Outputs, Position } from '@/types/utils'
 
@@ -35,39 +31,19 @@ const KWENTAStatsComponent = memo(function KWENTAStats({
   setValues,
   toTokenSymbol,
 }: Props) {
-  const { getTokenBySymbolAndChain } = useProtocols()
-  const fromTokenInfo = getTokenBySymbolAndChain(fromTokenSymbol, chainId.toString())
-  const toTokenInfo = getTokenBySymbolAndChain(toTokenSymbol, chainId.toString())
   // @todo: fetch marketKey by tokenSymbol
   const marketKey = FuturesMarketKey.sETHPERP
-  const marketAddress = contracts['KWENTA_PerpsV2Market'].address[chainId]
-
-  // fetches rates for all markets
-  const synthRates = useSynthsRates()
-  const ratesForCurrencies = useRatesForCurrencies()
-  const marketPrices = getMarketPrices(synthRates, ratesForCurrencies)
-  if (!marketPrices) {
-    throw `There was not possible to fetch Market Prices`
-  }
-
-  // fetches marketInfo and marketParameters for a marketKey
   const marketKeyBytes = formatBytes32String(marketKey)
-  const market = useFetchMarket(marketKeyBytes)
-  const currentRoundId = useFetchCurrentRoundId(marketKeyBytes)
-  const marketParameters = useFetchParameters(marketKeyBytes)
-  if (!currentRoundId) {
-    throw `There was not possible to fetch currentRoundId for market`
+  const marketData = useMarketInternalV2Data()
+  if (!marketData) {
+    throw `There was not possible to fetch data for market`
   }
-  if (!marketParameters) {
+  const marketParams = useMarketSettingsV2Parameters(marketKeyBytes)
+  if (!marketParams) {
     throw `There was not possible to fetch parameters for market`
   }
-  if (!market) {
-    throw `Market was not fetched`
-  }
-  const formattedMarket = formatFuturesMarket(market, currentRoundId, marketParameters)
 
-  const assetRate = marketPrices[toTokenInfo?.symbol as string]
-  const { oneHourlyFundingRate, skewAdjustedPrice } = extractMarketInfo(formattedMarket, assetRate)
+  const { oneHourlyFundingRate, skewAdjustedPrice } = extractMarketInfo(marketData, marketParams)
   if (!skewAdjustedPrice) {
     throw `There was not possible to fetch skew adjusted price`
   }
@@ -78,20 +54,21 @@ const KWENTAStatsComponent = memo(function KWENTAStats({
   const { marginDelta, nativeSizeDelta, sizeDelta } = formatOrderSizes(
     amount,
     leverage,
-    assetRate,
+    wei(marketData.assetPrice),
     position,
   )
 
-  const tradePreview = useGetTradePreview(
+  const tradePreview = useTradePreview(
+    chainId,
     sizeDelta,
     marginDelta,
-    marketKey,
-    marketAddress,
     position,
     leverage,
-    chainId,
+    marketData,
+    marketParams,
   )
   if (tradePreview.status !== 0) {
+    console.log(tradePreview.status)
     throw `There was not possible to fetch Position Stats`
   }
 
@@ -106,13 +83,13 @@ const KWENTAStatsComponent = memo(function KWENTAStats({
   setValues({
     protocol: 'kwenta',
     investmentTokenSymbol: 'sUSD',
-    fillPrice: wei(amount).mul(leverage).div(assetRate).toBN(),
+    fillPrice: wei(amount).mul(leverage).div(marketData.assetPrice).toBN(),
     priceImpact: positionStats.priceImpact.toBN(),
     protocolFee: positionStats.fee.add(KWENTA_FIXED_FEE).toBN(),
     tradeFee: positionStats.fee.toBN(),
     keeperFee: KWENTA_FIXED_FEE.toBN(),
     liquidationPrice: positionStats.liqPrice.toBN(),
-    oneHourFunding: assetRate.mul(oneHourlyFundingRate).toBN(),
+    oneHourFunding: wei(marketData.assetPrice).mul(oneHourlyFundingRate).toBN(),
   })
 
   return null
