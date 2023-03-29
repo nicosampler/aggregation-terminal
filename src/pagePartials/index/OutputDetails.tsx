11e59d4da673b708f802320dee7960c12b386192
Marcelo Rubini <rubini.marcelo@gmail.com>
2023-03-29 15:32:21 -0300

    fixes output details field comparison

--- src/pagePartials/index/OutputDetails.tsx
+++ src/pagePartials/index/OutputDetails.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { Tooltip } from '@/src/components/common/Tooltip'
 import { List, Stats } from '@/src/components/text/List'
 import { formatAmount } from '@/src/utils/GMX/format'
-import { Outputs } from '@/types/utils'
+import { Outputs, Position } from '@/types/utils'
 
 function setStyle(value?: BigNumber, comparison?: BigNumber) {
   if (!comparison || !value || value.eq(comparison)) {
@@ -33,8 +33,9 @@ type Props = {
   margin: BigNumber
   local: Outputs
   comparison?: Outputs
+  positionSide: Position
 }
-export function OutputDetails({ comparison, local, margin, tokenSymbol }: Props) {
+export function OutputDetails({ comparison, local, margin, positionSide, tokenSymbol }: Props) {
   const getTradeFeeText = () => {
     const text =
       local.protocol === 'kwenta'
@@ -57,7 +58,11 @@ export function OutputDetails({ comparison, local, margin, tokenSymbol }: Props)
           {formatAmount(margin, 18, 2)} {local.investmentTokenSymbol}
         </strong>
       </List>
-      <List as={motion.li} variants={itemVariants}>
+      <List
+        as={motion.li}
+        status={setStyle(comparison?.fillPrice, local.fillPrice)}
+        variants={itemVariants}
+      >
         <span>
           <Tooltip text="The estimated price at which the order will be executed.">
             Fill Price
@@ -65,7 +70,11 @@ export function OutputDetails({ comparison, local, margin, tokenSymbol }: Props)
         </span>
         <strong>{formatAmount(local.fillPrice)}</strong>
       </List>
-      <List as={motion.li} variants={itemVariants}>
+      <List
+        as={motion.li}
+        status={setStyle(comparison?.position, local.position)}
+        variants={itemVariants}
+      >
         <span>
           <Tooltip text="The estimated price at which the order will be executed.">
             Position
@@ -73,7 +82,11 @@ export function OutputDetails({ comparison, local, margin, tokenSymbol }: Props)
         </span>
         <strong>{formatAmount(local.position, 18, 2)}</strong>
       </List>
-      <List as={motion.li} variants={itemVariants}>
+      <List
+        as={motion.li}
+        status={setStyle(comparison?.orderSize, local.orderSize)}
+        variants={itemVariants}
+      >
         <span>
           <Tooltip text="The notional value of the position, expressed in the spot price of the token selected.">
             Size
@@ -105,11 +118,7 @@ export function OutputDetails({ comparison, local, margin, tokenSymbol }: Props)
         </span>
         <strong>{formatAmount(local.protocolFee)} </strong>
       </List>
-      <List
-        as={motion.li}
-        status={setStyle(local.tradeFee, comparison?.tradeFee)}
-        variants={itemVariants}
-      >
+      <List as={motion.li} variants={itemVariants}>
         <span>
           <Tooltip text={getTradeFeeText()}>Trade Fee </Tooltip>
         </span>
@@ -122,13 +131,25 @@ export function OutputDetails({ comparison, local, margin, tokenSymbol }: Props)
         <strong>{formatAmount(local.keeperFee)}</strong>
       </List>
 
-      <List as={motion.li} variants={itemVariants}>
+      <List
+        as={motion.li}
+        status={
+          positionSide == 'long'
+            ? setStyle(local.liquidationPrice, comparison?.liquidationPrice)
+            : setStyle(comparison?.liquidationPrice, local.liquidationPrice)
+        }
+        variants={itemVariants}
+      >
         <span>
           <Tooltip text="The price at which the position is liquidated.">Liquidation Price</Tooltip>
         </span>
         <strong>{formatAmount(local.liquidationPrice)}</strong>
       </List>
-      <List as={motion.li} variants={itemVariants}>
+      <List
+        as={motion.li}
+        status={setStyle(local.oneHourFunding, comparison?.oneHourFunding)}
+        variants={itemVariants}
+      >
         <span>
           <Tooltip text="Hourly payments to or from traders depending on their trade direction.">
             1H Funding
